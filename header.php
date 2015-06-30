1ed082a80713b3e810db96ab578db03b5cd2c132
root <root@rs.(none)>
2015-07-01 06:40:37 +0700

    menambahkan assets bootstrap

--- header.php
+++ header.php
@@ -1,6 +1,20 @@
 <!DOCTYPE html>
 <head>
-<title></title>
+<title><?php bloginfo('title'); ?> | <?php bloginfo('description'); ?></title>
+<meta name="author" content="oemunix"/>
+<meta name="title" content="<?php bloginfo('title'); ?>"/>
+<meta description content="<?php bloginfo('description'); ?>"/>
+<meta charset="uft-8"/>
+<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+<meta name="vieport" content="width=device-width, initial-scale=1"/>
 <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
+<!-- Navbar -->
+<div class="container">
+<div class="row">
+<div class="jumbotron">
+<h1>Hello World</h1>
+</div>
+</div>
+</div>
